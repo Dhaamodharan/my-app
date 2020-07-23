d65a19b1d18c7273510449791702cbeebf3ce2b7
Dhaamodharan <32461334+Dhaamodharan@users.noreply.github.com>
2020-07-23 08:54:39 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!- Welcome to DevOps Session";
+    private final String message = "Hello World!- Welcome to DevOps Session1";
 
     public App() {}
 
