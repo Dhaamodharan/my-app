45361ed7261a72d26fa96f7b0360a22c847c642b
Dhaamodharan <32461334+Dhaamodharan@users.noreply.github.com>
2020-12-03 08:21:19 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World India!";
+    private final String message = "Hello World India1!";
 
     public App() {}
 
